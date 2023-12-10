a61c14c228c8f14bf9cae55737040da81aefc702
Andrei.Petr <andreipetrovic@yandex.ru>
2023-12-10 11:31:21 -0800

    class mycontroller is created

---
+++ src/main/java/com/andreizez/spring/mvc_hibernate_aop/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.andreizez.spring.mvc_hibernate_aop.controller;
+
+
+import com.andreizez.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.andreizez.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+    @Autowired
+    private EmployeeDAO employeeDAO;
+    @RequestMapping("/")
+    public String showAllEMployees(Model model){
+        List<Employee> allEmployees=employeeDAO.getAllEmployees();
+        model.addAttribute("allEmps",allEmployees);
+        return "allEmployees";
+    }
+}
